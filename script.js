cd58915e4ac60f68ac19005b3da397e013da117f
Nicolas Storck Nickhorn <117923265+Nicolas-Nickhorn@users.noreply.github.com>
2022-12-04 21:45:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 function red(){
     document.getElementById("img-tenis").innerHTML = "<img src = 'imagens/tenis_red-removebg-preview.png'>"
     document.body.style.background = `linear-gradient(to right, rgb(240, 25, 25), rgba(114, 2, 2, 0.70))`
-    document.getElementById("img-model").innerHTML = ''
+    document.getElementById("valor").innerHTML = "R$99,90"
 }
 
 function blue(){
     document.getElementById("img-tenis").innerHTML = "<img src = 'imagens/tenis_blue-removebg-preview.png'>"
     document.body.style.background = `linear-gradient(to right, rgb(50, 20, 130), rgba(25, 5, 100, 0.10))`
+    document.getElementById("valor").innerHTML = "R$89,90"
 }
 
 function green(){
     document.getElementById("img-tenis").innerHTML = "<img src = 'imagens/tenis_green2-removebg-preview.png'>"
     document.body.style.background = `linear-gradient(to right, rgba(70, 240, 100, 0.50), rgb(5, 210, 40))`
+    document.getElementById("valor").innerHTML = "R$129,90"
 }
 
 function yellow(){
     document.getElementById("img-tenis").innerHTML = "<img src = 'imagens/tenis_yellow2-removebg-preview.png'>"
     document.body.style.background = `linear-gradient(to right, rgb(255, 255, 0), rgb(40, 5, 120))`
+    document.getElementById("valor").innerHTML = "R$109,90"
 }
